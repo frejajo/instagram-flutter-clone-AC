a534d010aaebcbb85583172064c63236254c27ce
frejajo <frejfrej@outlook.com>
2025-02-12 14:33:31 -0800

    text styles

---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+//making text styles pink to show updates!
+class AppTextStyles {
+  static const TextStyle h1 = TextStyle(
+    fontFamily: 'fonts/AvenirNextLTPro-Regular.otf',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    color: Colors.pink,
+    );
+
+  static const TextStyle h2 = TextStyle(
+    fontFamily: 'fonts/AvenirNextLTPro-Regular.otf',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: Colors.pink,
+  );
+
+  static const TextStyle h3 = TextStyle(
+    fontFamily: 'fonts/AvenirNextLTPro-Regular.otf',
+    fontSize: 12,
+    color: Colors.pink,
+  );
+  static const TextStyle inputField = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.normal,
+    color: Colors.pink,
+    fontFamily: 'fonts/AvenirNextLTPro-Regular.otf',
+  );
+
+  static const TextStyle inputFieldHint = TextStyle(
+    fontSize: 16.0,
+    color: Colors.pink,
+    fontFamily: 'fonts/AvenirNextLTPro-Regular.otf',
+  );
+}
+
+
